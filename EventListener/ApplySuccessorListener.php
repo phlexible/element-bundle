101260695c9006421c976d395a21c2f06a75072a
Stephan Wentz <swentz@brainbits.net>
2014-08-21 17:05:11 +0200

    fixes

--- EventListener/ApplySuccessorListener.php
+++ EventListener/ApplySuccessorListener.php
@@ -62,72 +62,6 @@ class ApplySuccessorListener
             )
         );
 
-        // TODO: TreeBundle
-        $this->connection->update(
-            'tree',
-            array(
-                'modify_user_id' => $toUid,
-            ),
-            array(
-                'modify_user_id' => $fromUid
-            )
-        );
-
-        // TODO: TreeBundle
-        $this->connection->update(
-            'tree_history',
-            array(
-                'create_user_id' => $toUid,
-            ),
-            array(
-                'create_user_id' => $fromUid
-            )
-        );
-
-        // TODO: TreeBundle
-        $this->connection->update(
-            'tree_online',
-            array(
-                'publish_uid' => $toUid,
-            ),
-            array(
-                'publish_uid' => $fromUid
-            )
-        );
-
-        // TODO: TeaserBundle
-        $this->connection->update(
-            'teaser',
-            array(
-                'modify_user_id' => $toUid,
-            ),
-            array(
-                'modify_user_id' => $fromUid
-            )
-        );
-
-        // TODO: TeaserBundle
-        $this->connection->update(
-            'teaser_history',
-            array(
-                'create_user_id' => $toUid,
-            ),
-            array(
-                'create_user_id' => $fromUid
-            )
-        );
-
-        // TODO: TeaserBundle
-        $this->connection->update(
-            'teaser_online',
-            array(
-                'publish_user_id' => $toUid,
-            ),
-            array(
-                'publish_user_id' => $fromUid
-            )
-        );
-
         $this->connection->update(
             'element_version',
             array(
@@ -138,5 +72,4 @@ class ApplySuccessorListener
             )
         );
     }
-
 }
